07f4501f7fcea8c43afc302abbbc5fdc81cd4465
Gothax <scott990328@gmail.com>
2024-06-30 03:02:05 +0900

    test coverage 100

--- hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,4 +13,13 @@ public class HelloController {
         model.addAttribute("data", "나");
         return "hello";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name;
+    }
+
+    
 }
+
