5dac5f241f9f7d8c825fe3614ef935b4ac6420fe
Gothax <scott990328@gmail.com>
2024-07-06 00:08:36 +0900

    DI

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,15 +4,24 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 import java.net.DatagramSocket;
 
 public class OrderServiceImpl implements OrderService{
-    private final MemoryMemberRepository memberRepository = new MemoryMemberRepository(); // OCP 위반
-    private DiscountPolicy discountPolicy;
+//    private final MemoryMemberRepository memberRepository = new MemoryMemberRepository(); // OCP 위반
+//    private DiscountPolicy discountPolicy;
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // DIP 위반
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 구현체를 바꾸기 위해 코드를 수정 -> OCP 위반
+
+    private final MemberRepository memberRepository; // OCP 위반
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- core/src/test/java/hello/core/CoreApplicationTests.java
+++ core/src/test/java/hello/core/CoreApplicationTests.java
@@ -3,14 +3,15 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
-	MemberService memberService = new MemberServiceImpl();
+	AppConfig appConfig = new AppConfig();
+	MemberService memberService = appConfig.memberService();
+
 	@Test
 	void join(){
 		//given
