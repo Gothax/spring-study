7a2a8d2f822222873092983f5693fb0fcf944c6b
Gothax <scott990328@gmail.com>
2024-07-05 19:13:43 +0900

    add

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+
+import java.net.DatagramSocket;
+
+public class OrderServiceImpl implements OrderService{
+    private final MemoryMemberRepository memberRepository = new MemoryMemberRepository(); // OCP 위반
+    private DiscountPolicy discountPolicy;
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // DIP 위반
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 구현체를 바꾸기 위해 코드를 수정 -> OCP 위반
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
--- core/src/test/java/hello/core/CoreApplicationTests.java
+++ core/src/test/java/hello/core/CoreApplicationTests.java
@@ -1,13 +1,25 @@
 package hello.core;
 
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
-
+	MemberService memberService = new MemberServiceImpl();
 	@Test
-	void contextLoads() {
+	void join(){
+		//given
+		Member member = new Member(1L, "memberA", Grade.VIP);
+		//when
+		memberService.join(member);
+		Member findMember = memberService.findMember(1L);
+		//then
+		Assertions.assertThat(member).isEqualTo(findMember);
 	}
 
 }
