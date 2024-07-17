71a69f8118f87f7fc41d53cb825b391509979394
Gothax <scott990328@gmail.com>
2024-07-18 02:02:53 +0900

    embedded 객체 추가, 수정

--- ex1-hello-jpa-start/src/main/java/domain/Delivery.java
+++ ex1-hello-jpa-start/src/main/java/domain/Delivery.java
@@ -1,9 +1,6 @@
 package domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class Delivery {
@@ -13,8 +10,40 @@ public class Delivery {
     @OneToOne(mappedBy = "delivery")
     private Order order;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
+
     private DeliveryStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
 }
--- ex1-hello-jpa-start/src/main/java/domain/Member.java
+++ ex1-hello-jpa-start/src/main/java/domain/Member.java
@@ -2,6 +2,7 @@ package domain;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +15,11 @@ public class Member {
 
     private String name;
 
-    private String city;
+    @Embedded
+    private Period period;
 
-    private String street;
-
-    private String zipcode;
+    @Embedded
+    private Address address;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
@@ -40,28 +41,27 @@ public class Member {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public Period getPeriod() {
+        return period;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setPeriod(Period period) {
+        this.period = period;
     }
 
-    public String getStreet() {
-        return street;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
-    public String getZipcode() {
-        return zipcode;
+    public List<Order> getOrders() {
+        return orders;
     }
 
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
-
 }
