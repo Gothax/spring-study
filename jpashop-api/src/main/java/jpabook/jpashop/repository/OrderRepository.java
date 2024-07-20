0a24cee3907f4f41ba46e181e0f372b18795b070
Gothax <scott990328@gmail.com>
2024-07-20 21:34:16 +0900

    fetch join 최적화 api 추가

--- jpashop-api/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop-api/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -75,6 +75,28 @@ public class OrderRepository {
         ).getResultList();
     }
 
+    public List<Order> finAllByItem() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        )
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
     /**
      * jpa에서 동적 쿼리를 해결하는 방법 - query dsl
      */
