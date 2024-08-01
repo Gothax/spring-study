1ddda83b5c8b7027c72b1622b59c2e7bca6fe655
Gothax <scott990328@gmail.com>
2024-08-02 01:09:43 +0900

    mvc 구조 개선 V4/ controller에서 modelview 반환하지 않고 viewName반환 - model 정보는 parameter로 전달

--- java_mvc_1/servlet/src/main/java/hello/servlet/web/frontcontroller/ModelView.java
+++ java_mvc_1/servlet/src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -9,11 +9,11 @@ import java.util.Map;
 @Getter @Setter
 public class ModelView {
 
+    private String viewName;
+    private Map<String, Object> model = new HashMap<>();
+
     public ModelView(String viewName) {
         this.viewName = viewName;
     }
-
-    private String viewName;
-    private Map<String, Object> model = new HashMap<>();
 
 }
