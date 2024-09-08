acf7dd7a55f0e505f77b45c2bbca646d236db3e9
Gothax <scott990328@gmail.com>
2024-09-08 20:53:47 +0900

    .

--- java-adv1/src/Main.java
+++ java-adv1/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("\"오랜만에 java\" = " + "오랜만에 java");
-        System.out.println("\"이번 주말에 이거 마무리\" = " + "이번 주말에 이거 마무리");
-        System.out.println("\"이번 주말에 이거 마무리\" = " + "이번 주말에 이거 마무리");
-        System.out.println("\"이번 주말에 이거 마무리\" = " + "이번 주말에 이거 마무리");
     }
 }
