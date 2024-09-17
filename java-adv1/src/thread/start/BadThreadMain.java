eaaee3d07be971e61d3070f7283fd5540954445b
Gothax <scott990328@gmail.com>
2024-09-18 00:58:27 +0900

    잘못된 thread run - start로 해야함

---
+++ java-adv1/src/thread/start/BadThreadMain.java
@@ -0,0 +1,20 @@
+package thread.start;
+
+public class BadThreadMain {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + ": main() start");
+
+        HelloThread helloThread = new HelloThread();
+        System.out.println(Thread.currentThread().getName() + ": start() 호출 전");
+
+        // 잘못된 예
+        helloThread.run(); // main 스레드가 run method 호출
+
+        System.out.println(Thread.currentThread().getName() + ": start() 호출 후");
+
+
+        System.out.println(Thread.currentThread().getName() + ": main() end");
+
+    }
+}
