195d877a92d7d46a880c4b0984e013503e0ac61b
Gothax <scott990328@gmail.com>
2024-10-01 12:47:42 +0900

    add comment at daemon thread

--- java-adv1/src/thread/start/DaemonThreadMain.java
+++ java-adv1/src/thread/start/DaemonThreadMain.java
@@ -5,6 +5,7 @@ public class DaemonThreadMain {
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName() + ": main start");
 
+        // daemon end는 출력되지 않고 main이 종료되며 종료됨
         DaemonThread daemonThread = new DaemonThread();
         daemonThread.setDaemon(true); // 데몬 스레드 여부 - 기본은 사용자 스레드
         daemonThread.start();
