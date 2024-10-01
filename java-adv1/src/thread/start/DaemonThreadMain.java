e22d81c46723511dde6cf5f13cafbf16e30f9cd2
Gothax <scott990328@gmail.com>
2024-10-01 12:44:28 +0900

    change typo

--- java-adv1/src/thread/start/DaemonThreadMain.java
+++ java-adv1/src/thread/start/DaemonThreadMain.java
@@ -1,6 +1,6 @@
 package thread.start;
 
-public class DeaemonThreadMain {
+public class DaemonThreadMain {
 
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName() + ": main start");
