4ccec3ab033eaa088197d63ae90e9b0c528e3fab
Gothax <scott990328@gmail.com>
2024-10-12 22:13:34 +0900

    change name

--- discoveryservice/src/main/java/com/gothaxcity/discoveryservice/DiscoveryService.java
+++ discoveryservice/src/main/java/com/gothaxcity/discoveryservice/DiscoveryService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiscoveryserviceApplication {
+public class DiscoveryService {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryserviceApplication.class, args);
+        SpringApplication.run(DiscoveryService.class, args);
     }
 
 }
--- discoveryservice/src/test/java/com/gothaxcity/discoveryservice/DiscoveryServiceTests.java
+++ discoveryservice/src/test/java/com/gothaxcity/discoveryservice/DiscoveryServiceTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryserviceApplicationTests {
+class DiscoveryServiceTests {
 
     @Test
     void contextLoads() {
