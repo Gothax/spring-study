afbf79670eed4ff58926597b685df25aff1e8749
Gothax <scott990328@gmail.com>
2024-10-15 23:05:55 +0900

    .

--- discoveryservice/src/main/java/com/gothaxcity/discoveryservice/DiscoveryService.java
+++ discoveryservice/src/main/java/com/gothaxcity/discoveryservice/DiscoveryService.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DiscoveryService {
 
+
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryService.class, args);
     }
