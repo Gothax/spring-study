186f0aedf54e7a17d16c07a1eb568cc8166502b2
Gothax <scott990328@gmail.com>
2024-10-21 20:47:16 +0900

    주석

--- discoveryservice/src/main/java/com/gothaxcity/discoveryservice/DiscoveryService.java
+++ discoveryservice/src/main/java/com/gothaxcity/discoveryservice/DiscoveryService.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DiscoveryService {
 
     public static void main(String[] args) {
+        // 중간고사 끝나고 ~ 기말 전까지 끝내자
         SpringApplication.run(DiscoveryService.class, args);
     }
 
