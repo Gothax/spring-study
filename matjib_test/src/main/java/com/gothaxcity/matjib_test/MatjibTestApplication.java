fbb5ce20da9af76c8794642e1f78f0e3e0d377f0
Gothax <scott990328@gmail.com>
2024-10-28 18:52:01 +0900

    naver api 정상 작동

--- matjib_test/src/main/java/com/gothaxcity/matjib_test/MatjibTestApplication.java
+++ matjib_test/src/main/java/com/gothaxcity/matjib_test/MatjibTestApplication.java
@@ -3,14 +3,22 @@ package com.gothaxcity.matjib_test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class MatjibTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MatjibTestApplication.class, args);
+        NaverApiSearch naverApiSearch = new NaverApiSearch();
 
-
-
+        naverApiSearch.search("홍대맛집");
     }
 
 }
