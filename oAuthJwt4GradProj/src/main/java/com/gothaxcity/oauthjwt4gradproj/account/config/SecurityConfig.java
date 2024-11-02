8a89618c2a98f9c5b0fec81966bcd8c25bf4a52a
Gothax <scott990328@gmail.com>
2024-11-02 19:12:41 +0900

    feat: security config
    구현필요
    - CustomOauth2UserService
    - custom success handler
    - token auth filter
    - token exception filter
    - auth entry point
    - deny handler

--- oAuthJwt4GradProj/src/main/java/com/gothaxcity/oauthjwt4gradproj/account/config/SecurityConfig.java
+++ oAuthJwt4GradProj/src/main/java/com/gothaxcity/oauthjwt4gradproj/account/config/SecurityConfig.java
@@ -1,4 +1,76 @@
 package com.gothaxcity.oauthjwt4gradproj.account.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    private final CustomOauth2UserService Oauth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final TokenAuthFilter tokenAuthFilter;
+
+    // security 적용X 리소스
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers("/error", "/favicon.ico");
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable) // cookie 사용안하니 disable
+                .cors(AbstractHttpConfigurer::disable) // TODO: 베포단계 cors 설정
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+
+
+                // 경로별 인가
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                new AntPathRequestMatcher("/"),
+                                new AntPathRequestMatcher("/auth/success")
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+
+
+                // oauth 설정
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(config -> config.userService(Oauth2UserService))
+                        .successHandler(customSuccessHandler)
+                )
+
+
+                // jwt 설정
+                .addFilterAt(tokenAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new TokenExceptionFilter(), tokenAuthFilter.class)
+
+
+                // 예외처리
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                        .accessDeniedHandler(new CustomAccessDeniedHandler())
+                );
+
+
+        return httpSecurity.build();
+    }
+
+
 }
