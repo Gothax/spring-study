cdc951e35589cfd613b3a4f1b49c3376f0b823d6
Gothax <scott990328@gmail.com>
2024-11-02 18:22:36 +0900

    init

---
+++ oAuthJwt4GradProj/src/main/java/com/gothaxcity/oauthjwt4gradproj/OAuthJwt4GradProjApplication.java
@@ -0,0 +1,13 @@
+package com.gothaxcity.oauthjwt4gradproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthJwt4GradProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthJwt4GradProjApplication.class, args);
+    }
+
+}
---
+++ oAuthJwt4GradProj/src/test/java/com/gothaxcity/oauthjwt4gradproj/OAuthJwt4GradProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gothaxcity.oauthjwt4gradproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthJwt4GradProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
