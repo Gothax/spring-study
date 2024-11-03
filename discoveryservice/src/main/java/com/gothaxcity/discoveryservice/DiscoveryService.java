ea6df985691e4103edf32edcc1b2907e524b6d7b
Gothax <scott990328@gmail.com>
2024-11-03 23:30:30 +0900

    .

--- discoveryservice/src/main/java/com/gothaxcity/discoveryservice/DiscoveryService.java
+++ discoveryservice/src/main/java/com/gothaxcity/discoveryservice/DiscoveryService.java
@@ -8,7 +8,6 @@ public class DiscoveryService {
 
 
     public static void main(String[] args) {
-        // 중간고사 끝나고 ~ 기말 전까지 끝내자
         SpringApplication.run(DiscoveryService.class, args);
     }
 
