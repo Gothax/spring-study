04a3d199c99c4af0bb760124dd8fbdf0c25842e7
Gothax <scott990328@gmail.com>
2024-11-15 19:46:00 +0900

    feat: first-service init

---
+++ msa_ecommerce/api-gateway-service/src/test/java/com/gothaxcity/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gothaxcity.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
