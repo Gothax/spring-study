0e5859cfdefddde12e3ca8691d24769478d246e6
Gothax <scott990328@gmail.com>
2024-11-15 19:51:55 +0900

    feat: init second service

---
+++ msa_ecommerce/second-service/src/test/java/com/gothaxcity/secondservice/SecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gothaxcity.secondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
