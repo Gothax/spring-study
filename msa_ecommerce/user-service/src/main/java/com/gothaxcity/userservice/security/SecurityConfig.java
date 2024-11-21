d80618c662668e5746ac282d7626177182dc9cb8
Gothax <scott990328@gmail.com>
2024-11-21 14:00:10 +0900

    feat: security 의존성, config 추가
    - JWT 추가 필요

---
+++ msa_ecommerce/user-service/src/main/java/com/gothaxcity/userservice/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.gothaxcity.userservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] WHITE_LIST = {
+            "/users/**",
+            "/**"
+    };
+
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+//                .formLogin(AbstractHttpConfigurer::disable)
+//                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+
+
+                // 경로별 인가
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(WHITE_LIST).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return httpSecurity.build();
+    }
+
+}
