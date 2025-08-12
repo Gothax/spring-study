aa9fcdc4ca7e29832fa8e154813143ea2fdd7eb9
Gothax <scott990328@gmail.com>
2025-08-12 22:23:58 +0900

    자바 고급1: 체크 exception 예제

---
+++ java-adv1/src/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,21 @@
+package thread.control;
+
+import static util.ThreadUtils.sleep;
+
+public class CheckedExceptionMain {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    static class CheckRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            sleep(1000);
+        }
+
+    }
+
+}
