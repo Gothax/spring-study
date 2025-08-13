8c338c0b964d47949dbeb3b4ed17d107ecc57bfd
Gothax <scott990328@gmail.com>
2025-08-13 23:05:14 +0900

    자바 고급1: 인터럽트 - interrupt() 메서드의 문제점

--- java-adv1/src/thread/control/interrupt/ThreadStopMainV3.java
+++ java-adv1/src/thread/control/interrupt/ThreadStopMainV3.java
@@ -3,11 +3,12 @@ package thread.control.interrupt;
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
 
-public class ThreadStopMainV2 {
+public class ThreadStopMainV3 {
 
     /**
      * 스레드 작업을 중지하는 방법 - thread interrupt()
-     * sleep 상태일때는 즉각 예외로 반응하지만 while문 전체에서 반응하는 것은 아니라는 문제점이 있음
+     * my task 스레드에서 interrupt 상태를 변경하지 않기 때문애 자원을 정리하다가 interrupt ture를 보고 자원을 정리하지 못하는 문제가 발생할 수 있음
+     * 즉 interrupt의 목적을 달성했다면 정상으로 돌려야한다
      * @param args
      */
     public static void main(String[] args) {
@@ -16,7 +17,7 @@ public class ThreadStopMainV2 {
         Thread thread = new Thread(task, "myTaskThread");
         thread.start();
 
-        sleep(4000); // 4초 후에 작업 중지 요청
+        sleep(1000);
         log("작업 중단 지시 thread.interrupt() 호출");
         thread.interrupt(); // 작업 중지 요청
         log("work 스레드 인터럽트 상태1= " + thread.isInterrupted());
@@ -30,18 +31,12 @@ public class ThreadStopMainV2 {
         @Override
         public void run() {
 
-            try {
-
-                while (true) {
-                    log("작업 중");
-                    Thread.sleep(3000);
-                }
-
-            } catch (InterruptedException e) {
-                log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
-                log("interrupt message = " + e.getMessage());
-                log("state= " + Thread.currentThread().getState());
+            while (!Thread.currentThread().isInterrupted()) {
+                log("작업중");
             }
+
+            log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
+            log("state= " + Thread.currentThread().getState());
 
 
             log("자원 정리");
