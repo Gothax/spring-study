03d1de8c209dce21be09f3e999cab9b69f41d480
Gothax <scott990328@gmail.com>
2025-09-02 15:26:18 +0900

    자바 고급1: 동시성 컬렉션 - 이 개념들을 모두 적용한 자바가 제공하는 동시성 컬렉션

---
+++ java-adv1/src/thread/collection/java/SetMain.java
@@ -0,0 +1,29 @@
+package thread.collection.java;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class SetMain {
+
+    public static void main(String[] args) {
+
+        Set<Integer> copySet = new CopyOnWriteArraySet<>();
+        copySet.add(2);
+        copySet.add(1);
+        copySet.add(3);
+        copySet.add(1);
+        System.out.println(copySet.getClass().getSimpleName());
+        System.out.println(copySet);
+
+        Set<Integer> skipSet = new ConcurrentSkipListSet<>();
+        skipSet.add(3);
+        skipSet.add(2);
+        skipSet.add(1);
+        skipSet.add(1);
+        System.out.println(skipSet.getClass().getSimpleName());
+        System.out.println(skipSet);
+
+    }
+
+}
