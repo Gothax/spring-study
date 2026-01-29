d594163d94d9349ec2cd5fb31eaca250af6854a3
Gothax <scott990328@gmail.com>
2026-01-29 18:57:00 +0900

    자바 고급2: 네트워크 - 소켓을 통한 server-client tcp 연결

---
+++ java-adv2/src/network/tcp/v2/ClientV2.java
@@ -0,0 +1,56 @@
+package network.tcp.v2;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+import static util.MyLogger.log;
+
+public class ClientV2 {
+
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        log("클라이언트 시작");
+        Socket socket = new Socket("localhost", PORT );
+
+        DataInputStream input = getDataInputStream(socket);
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        log("소켓 연결: " + socket);
+
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("전송 문자:" );
+
+            // 서버에게 문자 보내기
+            String toSend = scanner.nextLine();
+            output.writeUTF(toSend);
+            log("client -> server: " + toSend);
+
+            if (toSend.equals("exit")) {
+                break;
+            }
+
+            // 서버로부터 문자 받기
+            String received = input.readUTF();
+            log("client <- server: " + received);
+
+
+        }
+
+        log("연결 종료: " + socket);
+        input.close();
+        output.close();
+        socket.close();
+    }
+
+    private static DataInputStream getDataInputStream(Socket socket) throws IOException {
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        return input;
+    }
+
+
+}
